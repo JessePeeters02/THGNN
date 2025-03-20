8d22a018470f1f985a25828d2692fb13e92d8a3b
JessePeeters02 <jesse.peeters@student.kuleuven.be>
2025-03-20 19:35:13 +0000

    Mar 20, 2025, 8:35 PM

--- utils/generate_relation.py
+++ utils/generate_relation.py
@@ -60,6 +60,7 @@ def load_stock_data(stock_data_path):
         stock_df = pd.read_csv(os.path.join(stock_data_path, stock_file))
         stock_df['Date'] = pd.to_datetime(stock_df['Date'])
         stock_data[stock_name] = stock_df
+        print(stock_data)
     return stock_data
 
 # Laad de stock data
