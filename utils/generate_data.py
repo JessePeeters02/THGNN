6be4207990d9cf8dc30b9aaad9c6c1acaa0342bb
JessePeeters02 <jesse.peeters@student.kuleuven.be>
2025-03-21 13:18:54 +0000

    Mar 21, 2025, 2:18 PM

--- utils/generate_data.py
+++ utils/generate_data.py
@@ -110,7 +110,7 @@ def fun(relation_dt, start_dt_month, end_dt_month, stock_data):
 
 for i in range(20, len(stock_trade_data)):
     end_data = stock_trade_data[i].strftime('%Y-%m-%d')
-    start_data = stock_trade_data[i - 20].strftime('%Y-%m-%d')
+    start_data = stock_trade_data[i - 20+1].strftime('%Y-%m-%d')
     fun(end_data, start_data, end_data, stock_data)
 
 
