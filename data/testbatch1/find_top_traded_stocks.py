0e082ee43214850b4b663a4d308f486084d758d3
JessePeeters02 <jesse.peeters@student.kuleuven.be>
2025-04-16 09:44:06 +0200

    add testbatch1

---
+++ data/testbatch1/find_top_traded_stocks.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+
+# Configuratie
+base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(f"base_path: {base_path}")
+stock_data_path = os.path.join(os.path.dirname(base_path), "portfolio_construction", "data", "NASDAQ_data_wTO")
+print(f"stock_data_path: {stock_data_path}")
+topN = 100  # Aantal aandelen om te selecteren
+output_path = os.path.join(base_path, "data", "testbatch1", f"top_{topN}_stocks.csv")  # Waar de resultaten worden opgeslagen
+print(f"output_path: {output_path}")
+
+def get_top_traded_stocks(stock_data_path, top_n):
+    stock_files = [f for f in os.listdir(stock_data_path) if f.endswith('.csv')]
+    stock_volumes = []
+
+    print("Analyseren van handelsvolumes...")
+    no_volume_counter = 0
+    for stock_file in tqdm(stock_files):
+        stock_name = stock_file.split('.')[0]
+        df = pd.read_csv(os.path.join(stock_data_path, stock_file))
+        
+        if 'Volume' not in df.columns:
+            print(f"Waarschuwing: Geen 'Volume'-kolom in {stock_file}")
+            continue
+        if (df['Volume'] == 0).any():  # Controleer of er minstens één dag is met volume = 0
+            # print(f"Waarschuwing: bevat ergens 0 volume {stock_file}")
+            no_volume_counter += 1
+            continue  # Sla dit aandeel over
+        
+        avg_volume = df['Volume'].mean()
+        stock_volumes.append((stock_name, avg_volume))
+
+    print(f"{no_volume_counter} stocks met ergens geen volume")
+    # Sorteer op volume (hoog naar laag) en selecteer de top N
+    stock_volumes.sort(key=lambda x: x[1], reverse=True)
+    top_stocks = [stock[0] for stock in stock_volumes[:top_n]]
+
+    # Sla resultaten op in een CSV
+    pd.DataFrame({"Stock": top_stocks}).to_csv(output_path, index=False)
+    print(f"Top {top_n} meest verhandelde aandelen opgeslagen in {output_path}")
+
+if __name__ == "__main__":
+    get_top_traded_stocks(stock_data_path, topN)
---
+++ data/testbatch1/prepare_stock_and_daily_data.py
@@ -0,0 +1,61 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+
+# Configuratie
+FEATURE_COLS = ['Open', 'High', 'Low', 'Close', 'Volume', 'Turnover']
+
+# Pad configuratie
+base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(f"base_path: {base_path}")
+data_path = os.path.join(base_path, "data", "testbatch1")
+print(f"data_path: {data_path}")
+stock_data_path = os.path.join(os.path.dirname(base_path), "portfolio_construction", "data", "NASDAQ_data_wTO")
+print(f"stock_data_path: {stock_data_path}")
+stock_data_output = os.path.join(data_path, "stockdata")
+print(f"stock_data_output: {stock_data_output}")
+daily_data_output = os.path.join(data_path, "dailydata")
+print(f"daily_data_output: {daily_data_output}")
+best_stocks_path = os.path.join(data_path, f"top_100_stocks.csv")  # Waar de resultaten worden opgeslagen
+print(f"best_stocks_path: {best_stocks_path}")
+
+
+def collect_topN_stock(input_path, output_path):
+    stock_files = [f for f in os.listdir(input_path) if f.endswith('.csv')]
+    best_stocks_csv = pd.read_csv(best_stocks_path)
+    best_stocks = [f for f in best_stocks_csv['Stock']]
+    stock_counter = 0
+
+    for stock_file in tqdm(stock_files, desc="Transforming data structure"):
+        stock_name = stock_file.split('.')[0]
+        if stock_name in best_stocks:
+            stock_counter += 1
+            stock_df = pd.read_csv(os.path.join(input_path, stock_file))
+            stock_df.to_csv(os.path.join(output_path, f"{stock_name}.csv"), index=False)
+    print(f"Stock counter: {stock_counter}")
+
+
+def transform_to_daily_structure(input_path, output_path):
+    stock_files = [f for f in os.listdir(input_path) if f.endswith('.csv')]
+    stock_data = []
+
+    for stock_file in tqdm(stock_files, desc="Transforming data structure"):
+
+        stock_name = stock_file.split('.')[0]
+        stock_df = pd.read_csv(os.path.join(input_path, stock_file))
+        stock_df['Date'] = pd.to_datetime(stock_df['Date'])
+        stock_df['Stock'] = stock_name
+    
+        stock_data.append(stock_df[['Date', 'Stock'] + FEATURE_COLS])
+        # print(stock_data)
+
+    combined_df = pd.concat(stock_data, ignore_index=True)
+
+    # Sla op per dag
+    for date, group in tqdm(combined_df.groupby('Date'), desc="Saving daily files"):
+        date_str = date.strftime('%Y-%m-%d')
+        group.to_csv(os.path.join(output_path, f"{date_str}.csv"), index=False)
+
+
+collect_topN_stock(stock_data_path, stock_data_output)
+transform_to_daily_structure(stock_data_output, daily_data_output)
